1708ce4d1d78bb4cd2936effaa2dbe04267fa973
LuisRdgz <luisrdgzalm@gmail.com>
2026-01-22 16:07:30 -0600

    feature: navbar responsiva

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,32 +1,34 @@
 const navbar = () => {
-  return `
-  
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    return `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">Hidden brand</a>
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="#">Home</a>
+          <ul class="navbar-nav mx-auto me-lg-auto text-center text-lg-start">
+              <li class="nav-item">
+              <a class="nav-link active" href="#">Productos</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" href="#">Nosotros</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" href="#">Contactanos</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" href="#">Cuenta</a>
+              </li>
+              <li class="nav-item">
+              <a class="navbar-brand" href="#">
+                  <img src="public/images/carritoBlanco.png" alt="carrito" width="25">
+              </a>
+              </li>
+          </div>
       </div>
     </nav>
-  `
+    `
 }
 
 export {navbar};
