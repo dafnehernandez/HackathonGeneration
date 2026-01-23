988fd8be606d0da5438d0caeb436675345bd861e
Dafne Hernandez <mcflurrycerezita@gmail.com>
2026-01-23 00:39:12 -0600

    refactor: estructuracion del proyecto orden para homogeneizar

--- src/js/initApp.js
+++
@@ -1,9 +0,0 @@
-import { registro } from "./components/registro.js";
-
-const initApp = () => {
-  console.log("App initialized");
-  const registroContainer = document.getElementById("registro");
-  registroContainer.innerHTML = registro();
-}
-
-export {initApp};
